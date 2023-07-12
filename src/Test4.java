3f934722720865e4fa031b65117625bf5491c4af
ryanzhao2 <ryan.zhao2@gmail.com>
2023-07-12 13:50:09 -0400

    my change

---
+++ src/Test4.java
@@ -0,0 +1,12 @@
+public class Test4 {
+    public static void main(String[] args) {
+
+        String exclaim = addExclamationPoint("hot dogs");
+        System.out.println(exclaim);
+    }
+    public static String addExclamationPoint(String s) {
+        return s + "!";
+
+
+    }
+}
